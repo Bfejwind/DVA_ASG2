b626b7fa35f38e1f07205b5fcc0db526a9576f3b
bfejwndbdu <S10268236@connect.np.edu.sg>
2026-08-07 18:28:32 +0800

    Ghost Character, Splines Package, Ghost WIP

---
+++ Assets/Scripts/Ghost/GhostBehaviour.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class GhostBehaviour : MonoBehaviour
+{
+    [SerializeField] private SplineAnimate L_R_spline;
+    [SerializeField] private SplineAnimate R_L_spline;
+}
